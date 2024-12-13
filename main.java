9ed85736955f84b90538f80b15980854604f4c1e
gkouakou <guy.mathieu.kouakou@outlook.fr>
2024-12-13 23:39:34 +0100

    Ajout de fonctions

--- main.java
+++ main.java
@@ -1,7 +1,11 @@
 public class main {
     int a = 0;
     int b = 0;
-     int calculProduit (int a ,int b) {
-         return  a * b ;
-     }
+    int calculProduit (int a ,int b) {
+        return  a * b ;
+    }
+
+    int calculSome (int a ,int b) {
+        return  a + b ;
+    }
 }
